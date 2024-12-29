04a075e9124dcfb4cdd6be85166733a62eb68f2f
mrenann <marcos.renann.br@gmail.com>
2024-12-29 16:22:41 -0300

    feat: add Setebit experience

--- composeApp/src/commonMain/kotlin/br/mrenann/dev/portfolio/ui/tabs/HomeTab.kt
+++ composeApp/src/commonMain/kotlin/br/mrenann/dev/portfolio/ui/tabs/HomeTab.kt
@@ -1,7 +1,6 @@
 package br.mrenann.dev.portfolio.ui.tabs
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -26,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
