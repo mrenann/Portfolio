470c9f3204d796f7291f700ec75f08b4273796fd
mrenann <marcos.renann.br@gmail.com>
2025-03-28 21:04:19 -0300

    feat: education

--- composeApp/src/commonMain/kotlin/br/mrenann/portfolio/education/presentation/EducationTab.kt
+++ composeApp/src/commonMain/kotlin/br/mrenann/portfolio/education/presentation/EducationTab.kt
@@ -1,9 +1,17 @@
 package br.mrenann.portfolio.education.presentation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.mrenann.portfolio.components.timeline.CoursesColumn
+import br.mrenann.portfolio.components.timeline.ProccessStage
+import br.mrenann.portfolio.components.timeline.ProccessStageStatus
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import compose.icons.FeatherIcons
@@ -27,7 +35,72 @@ class EducationTab() : Tab {
 
     @Composable
     override fun Content() {
-        Text("Education")
+        val education =
+            arrayOf(
+                ProccessStage(
+                    title = "Bachelor's Degree in Computer Science",
+                    description = "Universidade Federal do Cariri (UFCA)",
+                    intervalDate = "2019 - 2023",
+                    isEducation = true,
+                    status = ProccessStageStatus.FINISHED,
+                ),
+                ProccessStage(
+                    title = "Technical High School in IT",
+                    subtitle = "EEEP Professor Moreira de Sousa",
+                    intervalDate = "2016-2018",
+                    isEducation = true,
+                    status = ProccessStageStatus.FINISHED,
+                ),
+            )
+
+        val certifications =
+            arrayOf(
+                ProccessStage(
+                    title = "Santander CODERS MOBILE",
+                    subtitle = "Digital House Brasil",
+                    intervalDate = "2021",
+                    isEducation = true,
+                    status = ProccessStageStatus.FINISHED,
+                ),
+                ProccessStage(
+                    title = "Everis Kotlin Developer",
+                    subtitle = "DIO",
+                    intervalDate = "2021",
+                    isEducation = true,
+                    status = ProccessStageStatus.FINISHED,
+                ),
+                ProccessStage(
+                    title = "Front End Development",
+                    subtitle = "Digital House Brasil",
+                    intervalDate = "2021",
+                    isEducation = true,
+                    status = ProccessStageStatus.FINISHED,
+                ),
+                ProccessStage(
+                    title = "Application Development",
+                    subtitle = "Cel.Lep Idiomas",
+                    intervalDate = "2020",
+                    isEducation = true,
+                    status = ProccessStageStatus.FINISHED,
+                ),
+            )
+
+        Text(
+            modifier = Modifier.padding(horizontal = 12.dp),
+            text = "Education",
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
+        CoursesColumn(education)
+
+        Text(
+            modifier = Modifier.padding(horizontal = 12.dp),
+            text = "Certifications",
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
+
+        CoursesColumn(certifications)
     }
 
 }
