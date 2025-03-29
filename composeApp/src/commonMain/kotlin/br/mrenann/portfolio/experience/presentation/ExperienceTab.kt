ca936e86b67696c1aef515b02caa38aca615f267
mrenann <marcos.renann.br@gmail.com>
2025-03-28 21:04:23 -0300

    feat: experience

--- composeApp/src/commonMain/kotlin/br/mrenann/portfolio/experience/presentation/ExperienceTab.kt
+++ composeApp/src/commonMain/kotlin/br/mrenann/portfolio/experience/presentation/ExperienceTab.kt
@@ -1,9 +1,17 @@
 package br.mrenann.portfolio.experience.presentation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.mrenann.portfolio.components.timeline.CoursesColumn
+import br.mrenann.portfolio.components.timeline.ProccessStage
+import br.mrenann.portfolio.components.timeline.ProccessStageStatus
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import compose.icons.FeatherIcons
@@ -27,7 +35,47 @@ class ExperienceTab() : Tab {
 
     @Composable
     override fun Content() {
-        Text("Profile")
+        val courses =
+            arrayOf(
+                ProccessStage(
+                    title = "Android Developer",
+                    subtitle = "Setebit",
+                    description = "React Native",
+                    intervalDate = "2023",
+                    status = ProccessStageStatus.FINISHED,
+                ),
+                ProccessStage(
+                    title = "Android Developer",
+                    subtitle = "Stone CO",
+                    description = "Android | Kotlin | KMP | Jetpack Compose",
+                    intervalDate = "2021-2023",
+                    status = ProccessStageStatus.FINISHED,
+                ),
+                ProccessStage(
+                    title = "Mobile Developer",
+                    subtitle = "Calang.Io",
+                    description = "Android | Kotlin | Leadership",
+                    intervalDate = "2021-2022",
+                    status = ProccessStageStatus.FINISHED,
+                ),
+            )
+
+        Text(
+            modifier = Modifier.padding(horizontal = 12.dp),
+            text = "Experience",
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
+
+        CoursesColumn(courses)
+
+        Text(
+            modifier = Modifier.padding(horizontal = 12.dp),
+            text = "Projects",
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
+
     }
 
 }
